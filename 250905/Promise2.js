f2deb126b1a1940420d2a7304576449e2d545563
sssangho <sangho353@naver.com>
2025-09-05 18:22:25 +0900

    Upload:Promise

---
+++ 250905/Promise2.js
@@ -0,0 +1,44 @@
+function wakeUp() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("1. 일어났다");
+            resolve();
+        }, 1000);
+    });
+}
+
+function brushTeeth() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("2. 이를 닦았다");
+            resolve();
+        }, 1000);
+    });
+}
+
+function eatBreakfast() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("3. 아침을 먹었다");
+            resolve();
+        }, 1000);
+    });
+}
+
+function goToWork() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log("4. 출근했다");
+            resolve();
+        }, 1000);
+    });
+}
+
+async function startDay() {
+    await wakeUp();
+    await brushTeeth();
+    await eatBreakfast();
+    await goToWork();
+}
+
+startDay();
